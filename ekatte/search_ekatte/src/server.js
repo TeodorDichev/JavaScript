61ec4aa308dada383e39f73f6f266d75e7b731fb
Teodor Dichev <teodichev@gmail.com>
2025-11-20 17:39:22 +0200

    Renaming and updating readme

--- ekatte/search_ekatte/src/server.js
+++ ekatte/search_ekatte/src/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { Pool } from 'pg';
-import router from './routes/index.js';
+import router from './routes/router.js';
 import cors from 'cors';
 
 dotenv.config({path: '.env'});
